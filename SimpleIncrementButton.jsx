6d5246b96ade5d97470fd4e09c1d88b8f28c3396
Thomas Nix <tnix2@uncc.edu>
2018-01-22 20:43:44 -0500

    Divided into multiple components to demonstrate parent-to-child data communication.

--- SimpleIncrementButton.jsx
+++ SimpleIncrementButton.jsx
@@ -1,26 +1,51 @@
 // Create React class component
 class Button extends React.Component{
 
-	// Set state object with propery 
-	state = {buttonValue : 0};
-  
-	// Define function to increment value of state property on click event
-  incrementValue = () => {
-  	this.setState((prevState) => ({
-    	buttonValue : prevState.buttonValue + 1
-    }));
-  };
-
-	// Render button with onClick handler set to the function
+	// Define button class component with access to parent function incrementValue through alias onClickFunc
 	render(){
   	return (
-    	<button onClick={this.incrementValue}>
+    	<button onClick={this.props.onClickFunc}>
      		{/* Set value of button to state buttonValue property   */}
-      	{this.state.buttonValue}
+      	Increment
       </button>
     )
   }
 }
 
-// render button using ReactDOM.render() on mountNode (used on online REPL) 
-ReactDOM.render(<Button />, mountNode);
+// Define result class component with access to parent state.incrementedValue property through alias incrementedValue (on passed in props element)
+const Result = (props) => {
+	return(
+  	<div>{props.incrementedValue}</div>
+  )
+}
+
+// Define application class
+class App extends React.Component{
+
+	// Set state object with propery 
+	state = {buttonValue : 0};
+  
+	// Update state to increment counter on function call (onclick triggers this function in the Button class component)
+  incrementValue = () => {
+  	this.setState((prevState) => ({
+			// Increment based off previous state
+    	buttonValue : prevState.buttonValue + 1
+    }));
+  };
+  
+  
+	render(){
+  	return (
+			// React allows for one parent-level element to be defined in the return call     
+      
+			// Render both Button and Result class components as child components/elements with needed access to parent (App component) properties/methods
+    	<div>
+    	  <Button onClickFunc={this.incrementValue} />
+          <Result incrementedValue={this.state.buttonValue} />
+    	</div>
+    )
+  }
+}
+
+// Render App class component to mountNode (used in online REPL)
+ReactDOM.render(<App />, mountNode);
